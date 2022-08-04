d9bb27c6d9c3c0ea1c49b58927ffe723e86ac85c
Orlando0x <rodor29@gmail.com>
2022-08-04 11:22:10 -0400

    Fusionando todo en index (menus desplegables en el header) y agregando javascript

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
 const smallMenu = document.querySelector(".container");
 const emailUser = document.querySelector(".signin");
+const menuMobile = document.querySelector(".menu-img")
+const menuList = document.querySelector(".container-menuMobile")
+const cart = document.querySelector(".cart");
+const shoppCart = document.querySelector("aside");
 
+cart.addEventListener("click", toggleCart);
 emailUser.addEventListener("click", toggleMenu);
+menuMobile.addEventListener("click", toggleMenuMobile);
+
+
+function toggleCart() {
+
+    shoppCart.classList.toggle("inactive3");
+    menuList.classList.remove("inactive2");
+    smallMenu.classList.remove("inactive");
+
+}
+
+
+function toggleMenuMobile() {
+    menuList.classList.toggle("inactive2");
+    shoppCart.classList.add("inactive3");
+
+}
 
 function toggleMenu() {
     /* smallMenu.setAttribute("style", "display: block"); */
     smallMenu.classList.toggle("inactive");
+    shoppCart.classList.add("inactive3");
 }
 
